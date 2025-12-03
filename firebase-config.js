71891cce2a466a8bfd37b1c6274c3fc0e30d99f5
helmystor <m67544013@gmail.com>
2025-12-03 10:06:35 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyD4tDA25irl5qavqdMfJD9_e7AbMleUC6c",
+  authDomain: "helmy-f0cd2.firebaseapp.com",
+  projectId: "helmy-f0cd2",
+  storageBucket: "helmy-f0cd2.firebasestorage.app",
+  messagingSenderId: "383002018546",
+  appId: "1:383002018546:web:f5410ea854a3a007e8866b"
+};
